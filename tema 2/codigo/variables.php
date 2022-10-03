f6f90553c856e3cb0f18f31cd85719ca91093e8f
georgi <georgi.borale@educa.jcyl.es>
2022-10-03 07:22:30 +0000

    varibles referencia

---
+++ tema 2/codigo/variables.php
@@ -0,0 +1,23 @@
+<h2>valor y referencias</h2>
+
+<?php
+
+$var=1;
+$var1=$var;
+echo $var. "<br>";
+echo $var1. "<br>";
+$var1=$var1 +1;
+echo $var. "<br>";
+echo $var1. "<br>";
+
+
+echo "<br>";
+
+$var=1;
+$var1=&$var;
+echo $var. "<br>";
+echo $var. "<br>";
+$var1=$var1 +1;
+echo $var. "<br>";
+echo $var. "<br>";
+?>
