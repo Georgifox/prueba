5139e15903190c206726167b0cf797b9b2be0a54
georgi <georgi.borale@educa.jcyl.es>
2022-10-10 07:29:24 +0000

    10/10/2022 clase

---
+++ tema 2/codigo/operadores.php
@@ -0,0 +1,24 @@
+<h2>nave espacial</h2>
+<?php
+
+    $a="b";
+    $b="a";
+    var_dump($a<=>$b);
+
+
+    $a=5;
+    $b=2;
+    var_dump($a&$b);
+
+    //operadores de bits
+    /*  & AND
+        | OR
+        ^
+        ~ Navegacion
+        $a << $b multiplica por la segunda variable (añade un cero en notacion binaria)
+        $a >> $b divide por la segunda variable
+    */
+    
+        
+?>
+
