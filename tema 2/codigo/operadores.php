697ece325cb566ec0ccd4197d6c905ecc7906d38
georgi <georgi.borale@educa.jcyl.es>
2022-10-11 09:02:02 +0000

    11/10 ejercicios figuras

--- tema 2/codigo/operadores.php
+++ tema 2/codigo/operadores.php
@@ -10,6 +10,8 @@
     $b=2;
     var_dump($a&$b);
 
+    var_dump($a>>$b);
+
     //operadores de bits
     /*  & AND
         | OR
@@ -18,7 +20,22 @@
         $a << $b multiplica por la segunda variable (añade un cero en notacion binaria)
         $a >> $b divide por la segunda variable
     */
-    
-        
+
+
+?>
+
+<?php
+
+//ejercicio piramide asteriscos
+$filas=5;
+
+for(&blanco=1; i = $filas;$blanco++){
+    $blanco++
+    print"  ";
+    for ($i=0; $i < ; $i++) { 
+        # code...
+    }
+}
+
 ?>
 
